c41f967eddd211d7aab5fb4ff5ae47c7f9034393
tasneem96 <43231519+tasneem96@users.noreply.github.com>
2018-09-20 12:01:59 +0600

    Update SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,8 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);     // set the cells in the bitmap to match the cards
+		
         }
 
 		/// <summary>
@@ -40,6 +41,7 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
+				
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
